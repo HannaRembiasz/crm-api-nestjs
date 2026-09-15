e68544d2e50a68e7b15b0c4a174567a3caa3ebfb
Hanna Kaczyńska <hannakacz13@gmail.com>
2026-09-15 09:14:12 +0200

    Filter contacts

--- src/contacts/contacts.controller.ts
+++ src/contacts/contacts.controller.ts
@@ -1,15 +1,16 @@
-import { Controller, Get, Post, Body, Param, Patch, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Patch, Delete, Query } from '@nestjs/common';
 import { ContactsService } from './contacts.service.js';
 import { CreateContactDto } from './dto/create-contact.dto.js';
 import { UpdateContactDto } from './dto/update-contact.dto.js';
+import { ContactQueryDto } from './dto/contact-query.dto.js';
 
 @Controller('contacts')
 export class ContactsController {
   constructor(private readonly contactsService: ContactsService) {}
 
   @Get()
-  getAllContacts() {
-    return this.contactsService.getAllContacts();
+  getAllContacts(@Query() query: ContactQueryDto) {
+    return this.contactsService.getAllContacts(query);
   }
 
   @Get(':id')
--- src/contacts/contacts.service.ts
+++ src/contacts/contacts.service.ts
@@ -2,13 +2,32 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service.js';
 import { CreateContactDto } from './dto/create-contact.dto.js';
 import { UpdateContactDto } from './dto/update-contact.dto.js';
+import { ContactQueryDto } from './dto/contact-query.dto.js';
 
 @Injectable()
 export class ContactsService {
   constructor(private prisma: PrismaService) {}
 
-  getAllContacts() {
-    return this.prisma.client.orm.public.Contact.all();
+  getAllContacts(query: ContactQueryDto) {
+    let contacts = this.prisma.client.orm.public.Contact;
+
+    if (query.firstName) {
+      contacts = contacts.where((contact) =>
+        contact.firstName.ilike(`%${query.firstName}%`),
+      );
+    }
+    if (query.lastName) {
+      contacts = contacts.where((contact) =>
+        contact.lastName.ilike(`%${query.lastName}%`),
+      );
+    }
+    if (query.email) {
+      contacts = contacts.where((contact) =>
+        contact.email.ilike(`%${query.email}%`),
+      );
+    }
+
+    return contacts.all();
   }
 
   getContactById(id: number) {
